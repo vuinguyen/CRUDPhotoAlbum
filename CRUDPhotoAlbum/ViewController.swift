b7c25e74566dc113a24e4ed9e27f7e92f5590b1a
Vui Nguyen <vui_engineer@yahoo.com>
2020-01-22 18:18:57 -0700

    initial horizontal album scrolling

--- CRUDPhotoAlbum/ViewController.swift
+++ CRUDPhotoAlbum/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var scrollView: UIScrollView!
+
+  @IBOutlet weak var middleView: UIView!
+  var images = [UIImage]()
+  var photos = ["schnoodle1", "schnoodle2", "schnoodle3", "schnoodle4"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
+    loadImages()
+    if images.count == 0 {
+      print("there are no pictures!")
+      return
+    }
+
+    for i in 0..<images.count {
+        let imageView = UIImageView()
+        let x = middleView.frame.size.width * CGFloat(i)
+        //imageView.center = middleView.center
+        imageView.frame = CGRect(x: x, y: 0, width: middleView.frame.width, height: middleView.frame.height)
+     // imageView.frame.height = middleView.frame.height
+     // imageView.frame.width = middleView.frame.width
+
+
+        imageView.contentMode = .scaleAspectFit
+      //imageView.contentMode = .center
+
+        imageView.image = images[i]
+
+      scrollView.contentSize.width = scrollView.frame.size.width * CGFloat(i + 1)
+
+      //imageView.center = middleView.center
+      /*
+        imageView.contentMode = .center;
+        if (imageView.bounds.size.width > (images[i]).size.width && imageView.bounds.size.height > (images[i]).size.height) {
+          imageView.contentMode = .scaleAspectFit;
+        }
+ */
+        scrollView.addSubview(imageView)
+    }
+    
   }
 
+  func loadImages() {
+    for i in 0..<photos.count {
+      if let image = UIImage(named: photos[i]) {
+        images.append(image)
+      }
+    }
+  }
 
 }
 
