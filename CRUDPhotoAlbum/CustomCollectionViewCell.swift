9d9e21877e51b5ca317f23002f10d9f45491873f
Adrian Bolinger <adrian.bolinger@me.com>
2020-01-30 17:52:07 -0500

    Shell...need to get cell rendering properly

---
+++ CRUDPhotoAlbum/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  CRUDPhotoAlbum
+//
+//  Created by Adrian Bolinger on 1/30/20.
+//  Copyright © 2020 SunfishEmpire. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
