8d8da36b86c271ce2d40fc8bd053589d17664101
Vui Nguyen <vui_engineer@yahoo.com>
2020-01-31 14:16:31 -0700

    initial layout for collection tab

---
+++ CRUDPhotoAlbum/CustomCollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomCollectionViewController.swift
+//  CRUDPhotoAlbum
+//
+//  Created by Vui Nguyen on 1/31/20.
+//  Copyright © 2020 SunfishEmpire. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CRUDPhotoAlbum/TabBarController.swift
+++ CRUDPhotoAlbum/TabBarController.swift
@@ -10,22 +10,22 @@ import UIKit
 
 class TabBarController: UITabBarController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-      self.selectedIndex = 1
-    }
-    
+    // Do any additional setup after loading the view.
+    self.selectedIndex = 2
+  }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  /*
+   // MARK: - Navigation
+
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
 
 }
